c08de97922c16b799418fe2d16ce277603571d54
alyssakelley <abkelley97@gmail.com>
2020-05-19 20:53:49 -0700

    Editted the style for the charts

--- covid19_visualization/src/components/Chart/Chart.jsx
+++ covid19_visualization/src/components/Chart/Chart.jsx
@@ -4,16 +4,23 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Chart = ({ data, country }) => {
+const Chart = ({ data, country, graphType}) => {
     const [dailyData, setDailyData] = useState([]);
-
     useEffect(() => {
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData());
         }
         // console.log(initialDailyData);
         fetchAPI();
+        window.scrollTo(0, 0); // this prevents the webapge from scrolling to the top when the different graphs are selected
     }, []); // this will only happen once 
+
+    let countryName = country;
+    let message = `Current state in ${country}`;
+    if (!country) {
+        countryName = "Global";
+        message = "Current Global state";
+    }
 
     const lineChart = (
         dailyData[0] ? (
@@ -28,6 +35,12 @@ const Chart = ({ data, country }) => {
                         fill: true,
                     }, 
                     {
+                        data: dailyData.map((data) => data.recovered),
+                        label: 'Recovered',
+                        borderColor: 'green',
+                        fill: true,
+                    }, 
+                    {
                         data: dailyData.map((data) => data.deaths),
                         label: 'Deaths',
                         borderColor: 'red',
@@ -36,9 +49,31 @@ const Chart = ({ data, country }) => {
                     },
                     ],
                 }}
+                options=
+                {{
+                    scales: {
+                        yAxes: [{
+                          scaleLabel: {
+                            display: true,
+                            labelString: 'Number of cases per category'
+                          }
+                        }],
+                        xAxes: [{
+                          scaleLabel: {
+                            display: true,
+                            labelString: 'Date data was recieved'
+                          }
+                        }],
+                    },
+                    title: { display: true, text:`Line graph - ${message}`}     
+                }}
             />
-            ) : null
+            ) : "Line Graph Unavailable"
     );
+
+    console.log("Line - This is in Chart.js and this is data for infected: " + dailyData.map((data) => data.confirmed));
+    console.log("Line - This is in Chart.js and this is data for recovered: " + dailyData.map((data) => data.recovered));
+    console.log("Line - This is in Chart.js and this is data for deaths: " + dailyData.map((data) => data.deaths));
 
     const barChart = (
         data.confirmed ? (
@@ -51,26 +86,34 @@ const Chart = ({ data, country }) => {
                         backgroundColor: 
                         [
                             'rgba(0, 0, 255, 0.5', // infected = blue
-                            'rgba(255, 0, 0, 0.5', // recovered = green
-                            'rgba(0, 255, 0, 0.5', // deaths = red
+                            'rgba(0, 255, 0, 0.5', // recovered = green
+                            'rgba(255, 0, 0, 0.5', // deaths = red
                         ], 
                         data: [data.confirmed.value, data.recovered.value, data.deaths.value]
                     }]
                 }}
                 options=
                 {{
+                    scales: {
+                        yAxes: [{
+                          scaleLabel: {
+                            display: true,
+                            labelString: 'Number of cases per category'
+                          }
+                        }],
+                        },     
                     legend: { display: false},
-                    title: { display: true, text:`Current state in ${country}`}
+                    title: { display: true, text:`Bar Graph - ${message}`}
                 }}
             />
-        ) : null
+        ) : "Bar Graph Unavailable"
     );
 
     // if there is a country chosen, then we display a bar chart for that country
     // otherwise display the line chart for the global values
     return (
         <div className={styles.container}>
-            {country ? barChart : lineChart} 
+            {graphType=="barGraph" ? barChart : lineChart} 
         </div>
     )
 }
