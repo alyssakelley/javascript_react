94b8e475613fff8cd2a0d5c446e7a2490536a359
alyssakelley <abkelley97@gmail.com>
2020-06-05 21:02:32 -0700

    Adding Tableau map

--- covid19_visualization/src/App.js
+++ covid19_visualization/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
-import { fetchData } from './api';
+import { fetchData, fetchCountryHotSpots } from './api';
 import covid19Image from './images/covid19_image2.png';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 // import MapDisplay from './components/MapDisplay/MapDisplay.js';
+import tableau from 'tableau-api';  
 
 class App extends React.Component {
 
@@ -16,14 +17,32 @@ class App extends React.Component {
         data: {},
         country: '',
         graphType: '',
+        // countryHotSpotPercents: {},
     }
 
     // with componentDidMount, the async goes before it instead of after like usual functions
     async componentDidMount() {
+        this.initViz();
         const fetchedData = await fetchData(); // fetchData is an asyncronous function so it needs an await
-        console.log(fetchedData);
+        console.log("Fetched data " + fetchedData);
         this.setState({ data: fetchedData, graphType: "barGraph"});
+        // let fetchedCountryHotSpots = new Map();
+        // fetchedCountryHotSpots= await fetchCountryHotSpots();
+        // console.log("Fetched country hot spots " + fetchedCountryHotSpots);
+        // this.setState({ countryHotSpotPercents: fetchedCountryHotSpots});
     }
+
+    initViz() {  
+        console.log("Inside the VIZ INIT!!");
+        const vizUrl = 'https://public.tableau.com/shared/Y357XSK6K?:display_count=y&:origin=viz_share_link';
+        const options = {
+            hideTabs: true,
+            width: "1000px",
+            height: "800px"
+        };
+        const vizContainer = this.vizContainer;  
+        let viz = new window.tableau.Viz(vizContainer, vizUrl, options); 
+      } 
 
     handleCountryChange = async (country) => {
         // get the data
@@ -47,6 +66,11 @@ class App extends React.Component {
         })
     }
 
+    // pickTopHotSpots() {
+    //     console.log("Picking hot spots");
+    //     return Object.keys(this.countryHotSpotPercents).reduce((a, b) => this.countryHotSpotPercents[a] > this.countryHotSpotPercents[b] ? a : b);
+    // }
+
 
     render() {
         let message = "Note: There is no daily data available for recovered cases.";
@@ -66,8 +90,9 @@ class App extends React.Component {
                     </Button>
                 </ButtonGroup>
                 <Chart data = {data} country = {country} graphType = {graphType}/>
-                {/* <h1>Here goes the map</h1> */}
                 <a>{graphType=="lineGraph" ? message : null}</a>
+                {/* <h1>{console.log(this.pickTopHotSpots())}</h1> */}
+                <a ref={(div) => { this.vizContainer = div }}></a>
             </div>
         )
     }
